111341019555dfe38546318d63025c0086830e2d
Jean-Michel Picod <jmichel@google.com>
2020-02-25 09:27:57 +0100

    Debug why Github workflow is failing here

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -ex
 
 # Ensure the script doesn't fail on Github workflows
 export TERM=${TERM:-vt100}

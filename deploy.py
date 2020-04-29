ca15ea43235f06bc58a70833c28e8bb461938425
Jean-Michel Picod <jmichel@google.com>
2020-04-29 11:48:12 +0200

    Fix formatting

--- deploy.py
+++ deploy.py
@@ -342,8 +342,7 @@ class OpenSKInstaller:
     env["RUSTFLAGS"] = " ".join(rust_flags)
 
     command = [
-        "cargo",
-        "build", "--release", "--target={}".format(props.arch),
+        "cargo", "build", "--release", "--target={}".format(props.arch),
         "--features={}".format(",".join(self.args.features))
     ]
     if is_example:

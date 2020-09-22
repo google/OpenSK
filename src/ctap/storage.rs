ed8308df385e84ebb88239c7c45b6b2ad57aeaa7
mingxguo27 <mingxguo@google.com>
2020-09-22 18:45:19 +0000

    Reversed changes

--- src/ctap/storage.rs
+++ src/ctap/storage.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #[cfg(feature = "with_ctap2_1")]
 use crate::ctap::data_formats::{extract_array, extract_text_string};
 use crate::ctap::data_formats::{CredentialProtectionPolicy, PublicKeyCredentialSource};

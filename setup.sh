d70806672e0aa89c680d7d891f744d5381966a9e
Jean-Michel Picod <jmichel@google.com>
2021-08-17 12:26:26 +0200

    Fix worklow and setup.sh script to use the TOML rust-toolchain file

--- setup.sh
+++ setup.sh
@@ -37,9 +37,8 @@ check_command pip3
 source tools/gen_key_materials.sh
 generate_crypto_materials N
 
-rustup install $(head -n 1 rust-toolchain)
+rustup show
 pip3 install --user --upgrade 'tockloader==1.5' six intelhex
-rustup target add thumbv7em-none-eabi
 
 # Install dependency to create applications.
 mkdir -p elf2tab

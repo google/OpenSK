2716c89ada2ed48ec91f7668e8cbf79436bc9f1f
Fabian Kaczmarczyck <kaczmarczyck@google.com>
2023-04-11 11:48:47 +0200

    Makes our CredRandom derivation FIPS compliant
    This change breaks existing usage of CredRandom.

--- libraries/crypto/src/hkdf.rs
+++ libraries/crypto/src/hkdf.rs
@@ -17,6 +17,27 @@ use super::Hash256;
 
 const HASH_SIZE: usize = 32;
 
+/// Computes the HKDF with the given salt and 256 bit (one block) output.
+///
+/// # Arguments
+///
+/// * `ikm` - Input keying material
+/// * `salt` - Byte string that acts as a key
+/// * `info` - Optional context and application specific information
+///
+/// This implementation is equivalent to a standard HKD, with `salt` fixed at a length of
+/// 32 byte and the output length l as 32.
+pub fn hkdf_256<H>(ikm: &[u8], salt: &[u8; HASH_SIZE], info: &[u8]) -> [u8; HASH_SIZE]
+where
+    H: Hash256,
+{
+    let prk = hmac_256::<H>(salt, ikm);
+    // l is implicitly the block size, so we iterate exactly once.
+    let mut t = info.to_vec();
+    t.push(1);
+    hmac_256::<H>(&prk, t.as_slice())
+}
+
 /// Computes the HKDF with empty salt and 256 bit (one block) output.
 ///
 /// # Arguments
@@ -31,11 +52,7 @@ where
     H: Hash256,
 {
     // Salt is a zero block here.
-    let prk = hmac_256::<H>(&[0; HASH_SIZE], ikm);
-    // l is implicitly the block size, so we iterate exactly once.
-    let mut t = info.to_vec();
-    t.push(1);
-    hmac_256::<H>(&prk, t.as_slice())
+    hkdf_256::<H>(ikm, &[0; HASH_SIZE], info)
 }
 
 #[cfg(test)]
@@ -73,6 +90,41 @@ mod test {
             let info = [i as u8];
             assert_eq!(
                 &hkdf_empty_salt_256::<Sha256>(&ikm.as_bytes(), &info[..]),
+                array_ref!(okm, 0, 32)
+            );
+        }
+    }
+
+    #[test]
+    fn test_hkdf_256_sha256_vectors() {
+        // Test vectors generated as above, but with salt:
+        let test_okms = [
+            hex::decode("f9be72116cb97f41828210289caafeabde1f3dfb9723bf43538ab18f3666783a")
+                .unwrap(),
+            hex::decode("a2480a09c7349d76e459f98a8259da40544bfbd2930d357a0f3250ade0acf941")
+                .unwrap(),
+            hex::decode("3904f7bf3615df9512fc6b1af651ed69b43f7fad424f9c718aaab63f377a36b9")
+                .unwrap(),
+            hex::decode("a0027dcffb27d356317199c6e65f153a9286ba114aee2d3cf45bdba83cb7c065")
+                .unwrap(),
+            hex::decode("786d1f89f54668bac443cc6a8887c95d6fbde07702cb4c16d76c452e87c50f79")
+                .unwrap(),
+            hex::decode("8e9a5bdf362c5aec2c31a742dfebd0b7b56e16ab8408d9d0609a4fad06446875")
+                .unwrap(),
+            hex::decode("4a35d3d7c80ff4fab65f7e30d6b305fc7bb39ffe905aabedd6593354f86177b6")
+                .unwrap(),
+            hex::decode("b1121deabd8b4308f3805cda8af991ee976bd8e413bcb6a8dd3fc26ebe2312d2")
+                .unwrap(),
+        ];
+        for (i, okm) in test_okms.iter().enumerate() {
+            // String of number i.
+            let ikm = i.to_string();
+            // Bytestring of byte i.
+            let salt = [i as u8; 32];
+            // Byte i.
+            let info = [i as u8];
+            assert_eq!(
+                &hkdf_256::<Sha256>(&ikm.as_bytes(), &salt, &info[..]),
                 array_ref!(okm, 0, 32)
             );
         }

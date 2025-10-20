1362a4a434fc79bb3190ef3a914c6ca908f3bfba
Fabian Kaczmarczyck <kaczmarczyck@google.com>
2025-10-20 07:33:28 +0200

    Fixes the error code for blocked UV
    First, with an answer on the specification question,
    we change the comment on the error code being a potential typo:
    https://github.com/fido-alliance/fido-2-specs/issues/1672
    
    Also, I realized that the default was wrong before, so now the error
    code with `*_UV_*` is used for UV operations as intended.

--- libraries/opensk/src/ctap/fingerprint.rs
+++ libraries/opensk/src/ctap/fingerprint.rs
@@ -138,7 +138,7 @@ pub fn perform_built_in_uv<E: Env>(
     internal_retry: bool,
 ) -> CtapResult<()> {
     if storage::uv_retries(env)? == 0 {
-        return Err(Ctap2StatusCode::CTAP2_ERR_PIN_BLOCKED);
+        return Err(Ctap2StatusCode::CTAP2_ERR_UV_BLOCKED);
     }
     env.fingerprint().check_fingerprint_init()?;
     let result = check_fingerprint_loop(env, channel, internal_retry);
@@ -180,7 +180,7 @@ fn check_fingerprint_loop<E: Env>(
                     FingerprintCheckError::NoMatch | FingerprintCheckError::Other => {
                         storage::decr_uv_retries(env)?;
                         if storage::uv_retries(env)? == 0 {
-                            return Err(Ctap2StatusCode::CTAP2_ERR_PIN_BLOCKED);
+                            return Err(Ctap2StatusCode::CTAP2_ERR_UV_BLOCKED);
                         }
                         retries -= 1;
                         if retries == 0 {
@@ -446,7 +446,7 @@ mod test {
         {
             assert_eq!(
                 perform_built_in_uv(&mut env, DUMMY_CHANNEL, true),
-                Err(Ctap2StatusCode::CTAP2_ERR_PIN_BLOCKED)
+                Err(Ctap2StatusCode::CTAP2_ERR_UV_BLOCKED)
             );
         } else {
             assert_eq!(
